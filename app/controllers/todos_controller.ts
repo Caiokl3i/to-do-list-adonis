7cbf87b8dedf3658eed333b796a9ff6cfe8c9436
Caiokl3i <caio.victor.santos12@gmail.com>
2026-07-23 15:58:33 -0400

    feat: cria método update  no todo controller

--- app/controllers/todos_controller.ts
+++ app/controllers/todos_controller.ts
@@ -1,5 +1,5 @@
 import TodoTransformer from '#transformers/todo_transformer'
-import { createTodoValidator } from '#validators/todo'
+import { createTodoValidator, updateTodoValidator } from '#validators/todo'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class TodosController {
@@ -29,4 +29,16 @@ export default class TodosController {
     return serialize(TodoTransformer.transform(todo))
   }
 
+  async update({ auth, params, request, serialize }: HttpContext) {
+    const data = await request.validateUsing(updateTodoValidator)
+    
+    const user = auth.getUserOrFail()
+
+    const todo = await user.related('todos').query().where('id', params.id).firstOrFail()
+
+    todo.merge(data)
+    await todo.save()
+
+    return serialize(TodoTransformer.transform(todo))
+  }
 }
